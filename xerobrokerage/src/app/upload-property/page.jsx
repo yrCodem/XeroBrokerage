dc187b24789c578c71d441343be13a89fa417d70
umair-moon-malik <umairmalik0@proton.me>
2025-04-27 23:54:27 +0530

    basic commit

--- xerobrokerage/src/app/upload-property/page.jsx
+++ xerobrokerage/src/app/upload-property/page.jsx
@@ -124,7 +124,7 @@ export default function Upload() {
   }
 
   return (
-    <div className='min-h-screen bg-gradient-to-br from-blue-50 to-gray-100 py-12 px-4 sm:px-6 lg:px-8'>
+    <div className='min-h-fit py-12 px-4 sm:px-6 lg:px-8'>
       <div className='max-w-4xl mx-auto'>
         {/* Header */}
         <div className='text-center mb-10'>
